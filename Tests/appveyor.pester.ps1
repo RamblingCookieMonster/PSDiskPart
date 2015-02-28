722be50cda1fc3eee1f05d8ea7c390c6252f3bc8
ramblingcookiemonster <RamblingCookieMonster@users.noreply.github.com>
2015-02-28 11:09:51 -0500

    another go

--- Tests/appveyor.pester.ps1
+++ Tests/appveyor.pester.ps1
@@ -1,13 +1,16 @@
 $PSVersion = $PSVersionTable.PSVersion.Major
 $TestFile = "TestsResults$($PSVersionTable.PSVersion.Major).xml"
+#http://www.appveyor.com/docs/environment-variables
+$ProjectRoot = $ENV:APPVEYOR_BUILD_FOLDER
+
 Import-Module Pester
 
 #Run a test with the current version of PowerShell
     Write-Output "Testing with PowerShell $PSVersion"
-    $PesterOutput = Invoke-Pester -Path ".\Tests" -OutputFormat NUnitXml -OutputFile $TestFile -PassThru
+    $PesterOutput = Invoke-Pester -Path "$ProjectRoot\Tests" -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile" -PassThru
 
     #Upload test output - you can see results on the 'Test' page
-    (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path ".\$TestFile"))
+    (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path "$ProjectRoot\$TestFile"))
 
     if ($PesterOutput.FailedCount -gt 0) {
         throw "$($PesterOutput.FailedCount) tests failed for PowerShell $PSVersion."
